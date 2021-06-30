d42664f3a6061a2844ddc01d338cc492dbb1af23
Dan Machinez <machinezdanilo@gmail.com>
2021-06-30 14:26:52 -0300

    .

--- ex035.py
+++ ex035.py
@@ -1,4 +1,4 @@
-print('=-' * 12)
+''''print('=-' * 12)
 print('Analisador de Triângulos')
 print('=-' * 12)
 r1 = float(input('Primeiro segmento: '))
@@ -7,4 +7,5 @@ r3 = float(input('Terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima PODEM formar um triângulo!')
 else:
-    print('Os segmentos acima NÃO PODEM formar um triângulo!')
+    print('Os segmentos acima NÃO PODEM formar um triângulo!')'''
+
